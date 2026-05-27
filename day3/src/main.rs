4fe39dfb2edd48f7947318337c38126df30a1978
Alex <u.alexsanme@gmail.com>
2026-05-27 10:47:47 +0200

    finished day3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,7 +6,7 @@ so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
 */
 
 fn main() {
-    let mut f = File::open("./data/sample").expect("Error reading file");
+    let mut f = File::open("./data/input").expect("Error reading file");
     let mut content = String::new();
     let _ = f.read_to_string(&mut content);
 
@@ -18,26 +18,35 @@ fn main() {
     println!("{total_joltage}");
 }
 
-fn get_largest_combination(line: &str) -> u32 {
+fn get_largest_combination(line: &str) -> i64 {
     let mut largest_combination = 0;
-    let (mut m_c1, mut m_c2) = (' ', ' ');
-    for (i, c1) in line.chars().enumerate() {
-        if i == (line.len() - 1) {
-            break;
-        }
+    let total_characters = line.chars().count();
+    for (i, c1) in line.chars().enumerate().take(total_characters - 1) {
+        let current_largest_combination: i64 = line
+            .chars()
+            .zip(std::iter::repeat(c1))
+            .enumerate()
+            .filter(|combination| {
+                if i == combination.0 || i > combination.0 {
+                    false
+                } else {
+                    true
+                }
+            })
+            .map(|combination| {
+                let (c1, c2) = combination.1;
+                let current_combination = format!("{}{}", c2, c1);
+                current_combination.parse::<i64>().unwrap()
+            })
+            .max()
+            .unwrap();
 
-        let j = i + 1;
-        println!("{} - {}", i, j);
-        let c2 = line.chars().nth(j).unwrap();
-        let current_combination_str = format!("{}{}", c1, c2);
-        let current_combination = current_combination_str.parse::<u32>().unwrap();
-        if current_combination > largest_combination {
-            m_c1 = c1;
-            m_c2 = c2;
-            largest_combination = current_combination;
+        if current_largest_combination > largest_combination {
+            largest_combination = current_largest_combination;
         }
     }
 
-    println!("largest combination {}-{}", m_c1, m_c2);
+    // println!("line: {}", line);
+    // println!("\tcombination: {}", largest_combination);
     return largest_combination;
 }
